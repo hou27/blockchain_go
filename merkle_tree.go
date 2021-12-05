c23327b6f890896cdd7bffd5dbb1400ac65d5f0b
hou27 <ataj125@gmail.com>
2021-12-05 03:45:13 +0000

    Implement Merkle tree

--- merkle_tree.go
+++ merkle_tree.go
@@ -1,5 +1,7 @@
 package main
 
+import "crypto/sha256"
+
 // MerkleTree is the container for the tree. It holds a pointer to the root of the tree,
 // a list of pointers to the leaf nodes, and the merkle root.
 type MerkleTree struct {
@@ -15,7 +17,56 @@ type Node struct {
 	Parent		*Node
 	Left		*Node
 	Right		*Node
-	child		bool
-	duplicate	bool
 	Hash		[]byte
+}
+
+// Creates a new Merkle tree
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []*Node
+
+	// Make it even
+	if len(data)%2 != 0 {
+		data = append(data, data[len(data)-1])
+	}
+
+	// Hash transactions
+	for _, tx := range data {
+		node := NewMerkleNode(nil, nil, tx)
+		nodes = append(nodes, node)
+	}
+
+	for i := 0; i < len(data)/2; i++ {
+		var newLevel []*Node
+
+		for j := 0; j < len(nodes); j += 2 {
+			node := NewMerkleNode(nodes[j], nodes[j+1], nil)
+			newLevel = append(newLevel, node)
+		}
+
+		nodes = newLevel
+	}
+
+	mTree := MerkleTree{nodes[0], nodes[0].Hash, nodes}
+	return &mTree
+}
+
+// Creates a new Merkle tree node
+func NewMerkleNode(left, right *Node, data []byte) *Node {
+	mNode := Node{}
+
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		mNode.Hash = hash[:]
+	} else {
+		hashes := append(left.Hash, right.Hash...)
+		hash := sha256.Sum256(hashes)
+		mNode.Hash = hash[:]
+	}
+
+	mNode.Left = left
+	mNode.Right = right
+	left.Parent = &mNode
+	right.Parent = &mNode
+
+	return &mNode
 }
