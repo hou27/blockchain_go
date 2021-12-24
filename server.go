de8de1746f2bbeaa1aeca02158c09db7efc356c8
hou27 <ataj125@gmail.com>
2021-12-24 13:11:53 +0900

    Implement StartServer

--- server.go
+++ server.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+// Starts a node
+func StartServer(nodeID int) {
+	// Creates servers
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", nodeID))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Close Listener
+	defer ln.Close()
+
+	for {
+		// Wait for connection
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		go func(c net.Conn) {
+			// Echo data back to the client
+			io.Copy(c, c)		
+			c.Close()
+		}(conn)
+	}
+}
