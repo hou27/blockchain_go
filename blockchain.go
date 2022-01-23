6875f0c207f60ab45a2d9bcc76d543711fb03c30
hou27 <ataj125@gmail.com>
2022-01-23 16:47:05 +0900

    Modify output of ShowBlocks

--- blockchain.go
+++ blockchain.go
@@ -99,7 +99,7 @@ func (bc Blockchain) ShowBlocks() {
 		fmt.Printf("Data: %s\n", block.Data)
         fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
-		fmt.Printf("Prev Hash: %d\n", block.Nonce)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
 		fmt.Printf("is Validated: %s\n", strconv.FormatBool(pow.Validate()))
 	}
 }
